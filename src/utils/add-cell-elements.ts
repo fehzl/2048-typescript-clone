28251a3c77e070f6871d10621d1d51ccc72f7620
Felipe Feliciano <fehzl@live.com>
2022-03-27 18:50:45 +0000

    :sparkles: feat: add a function to add cell elements

---
+++ src/utils/add-cell-elements.ts
@@ -0,0 +1,10 @@
+export function addCellElements(gridElement: HTMLElement, GRID_SIZE: number) {
+  const cells = [];
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cells.push(cell);
+    gridElement.append(cell);
+  }
+  return cells;
+}

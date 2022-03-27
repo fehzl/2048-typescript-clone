40cf5b729ca15db07c4b763c5f13ae3e1e4e7106
Felipe Feliciano <fehzl@live.com>
2022-03-27 18:54:27 +0000

    :sparkles: feat: generate a Grid by class

--- src/view/App.ts
+++ src/view/App.ts
@@ -1,6 +1,16 @@
 import { html } from "lit-html";
+import { Grid } from "~/core/grid";
+import { Tile } from "~/core/tile";
+import mounted from "~/utils/mounted";
 import { Board } from "./board";
 
 export function App() {
+  mounted(() => {
+    const board = document.getElementById("board");
+    const grid = new Grid(board);
+    grid.randomEmptyCell().tile = new Tile(board);
+    grid.randomEmptyCell().tile = new Tile(board);
+  });
+
   return html`<div>${Board()}</div>`;
 }

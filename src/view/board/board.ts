feff5aff6246142e191bb85e6cf8c6a86baac84b
Felipe Feliciano <fehzl@live.com>
2022-03-27 18:52:43 +0000

    :fire: refactor: remove poc code

--- src/view/board/board.ts
+++ src/view/board/board.ts
@@ -2,23 +2,5 @@ import { html } from "lit-html";
 import "./board.scss";
 
 export function Board() {
-  return html`<div id="board">
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="tile">2</div>
-  </div>`;
+  return html`<div id="board"></div>`;
 }
